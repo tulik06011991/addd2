75ad4245ef72b4bafc39709dcb6aeca08987fc08
tulik <tolqinmirsaliyev@gmail.com>
2024-02-07 21:05:47 +0300

    o'zgardi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <h1></h1>
+    <>
+    <Navbar/>
+    <Routes>
+
+    </Routes>
+    </>
 
   )
 }
---
+++ src/components/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "react-bootstrap"
+
+const Register = () => {
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default Register
